eec8fe064c7740b422ec45475aa6bd44f3735e7d
ANANDNJIT <aplv180@gmail.com>
2023-11-29 20:51:54 -0500

    add the mutuations js file

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,23 +1,82 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+  
+  mutation LoginUser($email: String!, $password: String!) {
+    
+      login(email: $email, password: $password) {
+      token
+        user {
+           _id
+          username
+       }
     }
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
+export const ADD_USER = gql`
+       
+         mutation AddUser($username: String!, $email: String!, $password: String!) {
+
+
+    addUser(username: $username, email: $email, password: $password) {
+
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: BookInput!) {
+
+    saveBook(newBook: $newBook) {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+   mutation removeBook($bookId: ID!) {
+     removeBook(bookId: $bookId) {
+      _id
+      
+      username
+     
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,48 +1,60 @@
 
+
 const { gql } = require('apollo-server-express');
 
 
 const typeDefs = gql`
+        type Query {
+    me: User
+  }
+
+  type Mutation {
+      login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+     saveBook(bookInput: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+
+
   type User {
-    _id: ID!
+     _id: ID!
     username: String!
-    email:String!
-    bookCount:Int
-    savedBooks:  [Book]
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+
+
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+
+    image: String
+    link: String
   }
 
   type Auth {
-    token :ID!
+    token: String!
     user: User
   }
 
-  type Book {
-    bookId:ID!
+
+
+  input BookInput {
     authors: [String]
     description: String
-    title: String
-    image:String
-    link:String
-  }
-  input  SavedBookInput{
-        bookId: String
-        authors: [String]
-        title: String
-        description: String
-        image: String
-        link: String
-    }
-
-    type Query {
-        me: User
-    }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(newBook: SavedBookInput): User
-        removeBook(bookId: ID!): User
+    title: String!
+    bookId: ID!
+    image: String
+    link: String
   }
 `;
+
+
 
 module.exports = typeDefs;
