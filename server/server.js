e6b77ab915e9f923d42b0b454d2810e2e4aa7fa2
ANANDNJIT <aplv180@gmail.com>
2023-12-02 22:34:41 -0500

    update the  server file

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ async function startApolloServer() {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/index.html'));
     });
  // }
 
